20b7bf56dd95aada502a1d9fb79e24d79d1345eb
dennis pike <daspiker@gmail.com>
2021-06-28 22:42:50 -0400

    cron now running with test crontab

--- boot.sh
+++ boot.sh
@@ -2,6 +2,6 @@
 source venv/bin/activate
 flask db upgrade
 service cron start
-exec sudo -u fakie gunicorn -b :5000 --access-logfile - --error-logfile - fakie:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - fakie:app
 
 
